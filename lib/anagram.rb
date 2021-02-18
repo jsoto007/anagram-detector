28deb1b08480be93f2ba2f9ab70dad59c419bf16
Jose Soto <jsoto7087@gmail.com>
2021-02-18 15:07:52 -0500

    commit tree ff1aec1165866748aa114bd6ae18e48d7a5e76f5

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,26 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :match
+
+    def initialize(match)
+        @match = match
+    end 
+
+    def match(word)
+        word.select do |word|
+
+        is_match?(word)
+        end 
+    end 
+
+    def is_match?(word)
+        word.chars.sort == @match.chars.sort
+    end 
+    
+
+end 
+
+
+
+
+
